581fd6092f6f0df6d770b01c8f2229c80663203d
junior31064049@gmail.com <junior31064049@gmail.com>
2025-06-27 16:42:55 -0500

    add testimonials, benefit cards and featured products

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Footer from './components/pages/Footer'
 import Navbar from './components/pages/HomeNavbar'
+import Benefits from './pages/home/Benefits'
+import FeaturedProducts from './pages/home/FeaturedProducts'
+import Testimonials from './pages/home/Testimonials'
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
         <p className="font-serif">Hola</p>
         <p className="font-decorative">Hola</p>
       </main>
+      <FeaturedProducts />
+      <Benefits />
+      <Testimonials />
       <Footer />
     </div>
   )
