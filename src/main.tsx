1b5cb3af8f0180bde8be0041f30e53e98dda7d2e
junior31064049@gmail.com <junior31064049@gmail.com>
2025-07-16 15:10:55 -0500

    integrate tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,20 @@ import { RouterProvider } from 'react-router'
 import { Router } from './Router.tsx'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Toaster } from './components/ui/sonner.tsx'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={Router} />
-      <Toaster />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      {/* The rest of your application */}
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={Router} />
+        <Toaster />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>
 )
